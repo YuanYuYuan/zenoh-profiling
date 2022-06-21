b18c776724121cd545b942b499f7725e54ba6811
yuanyuyuan <az6980522@gmail.com>
2022-06-21 13:42:50 +0800

    rearrange z_eval & z_query

--- router-eval-test/src/bin/z_eval.rs
+++ router-eval-test/src/bin/z_eval.rs
@@ -6,8 +6,10 @@ use zenoh::{
 use async_std::stream::StreamExt;
 use clap::Parser;
 use rand::Rng;
-use rand::prelude::SliceRandom;
+// use rand::prelude::SliceRandom;
 use zenoh_protocol_core::WhatAmI;
+use futures::{future::{try_join_all, select}, FutureExt};
+use std::time::Duration;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -20,20 +22,31 @@ struct Args {
     #[clap(short, long)]
     disable_multicast: bool,
 
+    #[clap(short, long)]
+    disable_peers_autoconnect: bool,
+
     #[clap(short, long, default_value = "peer")]
     mode: WhatAmI,
 
     #[clap(short, long)]
     connect: Option<String>,
+
+    #[clap(short, long, default_value = "30")]
+    timeout: u64,
 }
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    // use this to capture warn & error while piping stdout/stderr to files
+    env_logger::init();
+
     let Args {
         num_peers,
         disable_multicast,
+        disable_peers_autoconnect,
         connect,
         mode,
+        timeout,
     } = Args::parse();
 
 
@@ -54,6 +67,12 @@ async fn main() -> Result<()> {
                         .set_enabled(Some(false))
                         .unwrap();
                 }
+                if disable_peers_autoconnect {
+                    config
+                        .scouting
+                        .set_peers_autoconnect(Some(false))
+                        .unwrap();
+                }
                 if let Some(x) = connect_ {
                     config
                         .connect
@@ -72,11 +91,12 @@ async fn main() -> Result<()> {
                 .kind(EVAL)
                 .await
                 .unwrap();
+            println!("[Eval] Peer #{} builds queryable at '/key/{}'", idx, idx);
             while let Some(query) = queryable.next().await {
-                println!("Received query: {}", query.selector());
+                println!("[Eval] Peer #{} received query {}", idx, query.selector());
                 query.reply_async(Sample::new(
                     key_expr.clone(),
-                    format!("Hi, I'm peer {}.", idx)
+                    format!("Hi, I'm peer #{}.", idx)
                 )).await;
             }
             // queryable.close();
@@ -85,7 +105,9 @@ async fn main() -> Result<()> {
         })
     });
 
-    futures::future::try_join_all(jobs).await?;
+    select(try_join_all(jobs), async_std::task::sleep(Duration::from_secs(timeout)).boxed()).await;
+
+    println!("[Eval] Ended.");
 
     Ok(())
 }
---
+++ router-eval-test/src/bin/z_query.rs
@@ -0,0 +1,88 @@
+use async_std::{stream::{IntoStream}, future::timeout};
+use futures::prelude::stream::{StreamExt, TryStreamExt};
+use clap::Parser;
+use zenoh::config::Config;
+use zenoh::prelude::SplitBuffer;
+use zenoh_protocol_core::WhatAmI;
+use std::time::Duration;
+
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    disable_multicast: bool,
+
+    #[clap(short, long, default_value = "peer")]
+    mode: WhatAmI,
+
+    #[clap(short, long)]
+    connect: Option<String>,
+
+    #[clap(short, long, default_value = "30")]
+    timeout: u64,
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    let Args {
+        disable_multicast,
+        connect,
+        mode,
+        timeout,
+    } = Args::parse();
+
+    let config = {
+        let mut config = Config::default();
+        if disable_multicast {
+            config.scouting.multicast.set_enabled(Some(false)).unwrap();
+        }
+        if let Some(x) = connect {
+            config.connect.endpoints.extend(vec![x.try_into()?]);
+        }
+
+        config.set_mode(Some(mode)).unwrap();
+        config
+    };
+
+    let session = zenoh::open(config).await?;
+    let num_replied = session
+        .get("/key/*")
+        .await?
+        .as_trystream()
+        .take_until(
+            async move {
+                async_std::task::sleep(Duration::from_secs(timeout)).await;
+            }
+        )
+        .and_then(|reply| async move {
+            println!(
+                "[Query] Received reply '{}' from '{}'",
+                String::from_utf8_lossy(&reply.sample.value.payload.contiguous()),
+                reply.sample.key_expr.as_str()
+            );
+            Result::<_, Error>::Ok(())
+        })
+        .count()
+        .await;
+
+    // let mut replies = session.get("/key/*").await?;
+    // while let Ok(reply) = timeout(Duration::from_secs(10), replies.next()).await {
+    //     dbg!();
+    //     if let Some(reply) = reply {
+    //         println!(
+    //             "[Query] Received reply '{}' from '{}'",
+    //             String::from_utf8_lossy(&reply.sample.value.payload.contiguous()),
+    //             reply.sample.key_expr.as_str()
+    //         )
+    //     }
+    //     dbg!();
+    // }
+    //
+
+    println!("[Query] Ended with {} peers replied.", num_replied);
+
+    Ok(())
+}

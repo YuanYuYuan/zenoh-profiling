fbd6fb7a55f0825b37b5cce34c9248979b00d68a
yuanyuyuan <az6980522@gmail.com>
2022-06-30 13:47:29 +0800

    update test.sh

--- router-eval-test/utils/test.sh
+++ router-eval-test/utils/test.sh
@@ -2,11 +2,12 @@
 
 ROUTER_PROGRAM_PATH="../../zenoh/target/release/zenohd"
 EVAL_PROGRAM_PATH="./target/release/z_eval"
+# EVAL_PROGRAM_PATH="cargo flamegraph --bin=z_eval -- "
 QUERY_PROGRAM_PATH="./target/release/z_query"
 
-ROUTER_PROGRAM="$(basename $ROUTER_PROGRAM_PATH)"
-EVAL_PROGRAM="$(basename $EVAL_PROGRAM_PATH)"
-QUERY_PROGRAM="$(basename $QUERY_PROGRAM_PATH)"
+ROUTER_PROGRAM="zenohd"
+EVAL_PROGRAM="z_eval"
+QUERY_PROGRAM="z_query"
 
 if ! command -v $ROUTER_PROGRAM_PATH &> /dev/null; then
     echo "$ROUTER_PROGRAM_PATH could not be found!"
@@ -63,12 +64,14 @@ EVAL_MODE="peer"
 # EVAL_MODE="client"
 EVAL_TIMEOUT=15
 QUERY_TIMEOUT=15
+# EVAL_TIMEOUT=30
+# QUERY_TIMEOUT=30
 
 export PYTHONWARNINGS="ignore"
 
 cleanup
 
-# for NUM_PEERS in {12..48}; do
+# for NUM_PEERS in {12..32}; do
 for NUM_PEERS in 24; do
     echo -n "Testing $NUM_PEERS peers ... "
     psrecord "
@@ -107,28 +110,29 @@ for NUM_PEERS in 24; do
     #     --plot ${EVAL_USAGE_DIR}/${NUM_PEERS}.png \
     #     --include-children > /dev/null &
 
-    sleep 3
+    # sleep 3
+    sleep 10
 
-    # psrecord "
-    #     $QUERY_PROGRAM_PATH \
-    #         --mode 'peer' \
-    #         --disable-multicast \
-    #         --timeout $QUERY_TIMEOUT \
-    #         --connect "$ENDPOINT" > ${QUERY_LOG_DIR}/${NUM_PEERS}.txt 2>&1
-    # " \
-    #     --log ${QUERY_USAGE_DIR}/${NUM_PEERS}.txt \
-    #     --plot ${QUERY_USAGE_DIR}/${NUM_PEERS}.png \
-    #     --include-children > /dev/null
+    psrecord "
+        $QUERY_PROGRAM_PATH \
+            --mode 'peer' \
+            --disable-multicast \
+            --timeout $QUERY_TIMEOUT \
+            --connect "$ENDPOINT" > ${QUERY_LOG_DIR}/${NUM_PEERS}.txt 2>&1
+    " \
+        --log ${QUERY_USAGE_DIR}/${NUM_PEERS}.txt \
+        --plot ${QUERY_USAGE_DIR}/${NUM_PEERS}.png \
+        --include-children > /dev/null
 
-    sleep 3
+    # sleep 3
 
     cleanup
     sleep 1
 
     NUM_REPLIES=$(cat ${QUERY_LOG_DIR}/${NUM_PEERS}.txt | grep "\[Query\] Ended with" | cut -d ' ' -f 4)
     if [ "$NUM_REPLIES" = "$NUM_PEERS" ]; then
-        echo "passed."
+        echo "passed." | tee -a ${QUERY_LOG_DIR}/${NUM_PEERS}.txt
     else
-        echo "failed."
+        echo "failed." | tee -a ${QUERY_LOG_DIR}/${NUM_PEERS}.txt
     fi
 done

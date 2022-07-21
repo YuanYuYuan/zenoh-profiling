09b5d7770a52b6d1b514bd940cc232d582a2b5c5
yuanyuyuan <az6980522@gmail.com>
2022-07-21 13:07:35 +0800

    finish reply number counting tool

--- router-eval-test/utils/count_recevied_reply.py
+++ router-eval-test/utils/count_recevied_reply.py
@@ -3,6 +3,8 @@
 import argparse
 from pathlib import Path
 import json
+import pandas as pd
+import plotly.express as px
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -19,8 +21,27 @@ num_peers = int(args.in_dir.stem.split('-')[-1])
 with open(args.in_dir / 'node_mapping.json') as f:
     node_mapping = json.load(f)
 
-counter = {pid: 0 for (pid, node_name) in node_mapping.items() if node_name not in ['RO', 'QU'] }
-print(counter)
-#  with open(args.in_dir / 'log/query.txt') as f:
-#      for line in f.readlines():
-#          if 'Received reply' in line:
+counter = {node_name: 0 for (_, node_name) in node_mapping.items() if node_name not in ['RO', 'QU'] }
+
+with open(args.in_dir / 'log/query.txt') as f:
+    for line in f.readlines():
+        if 'Received reply' in line:
+            pid = line.split('peer')[-1].split('.')[0].strip()
+            counter[node_mapping[pid]] += 1
+
+is_passed = True
+for val in counter.values():
+    if val == 0:
+        is_passed = False
+        break
+
+data = pd.DataFrame({'peer': counter.keys(), 'count': counter.values()})
+fig = px.bar(
+    data,
+    x='peer',
+    y='count',
+    labels={'peer': 'Peer', 'count': 'Count'},
+    title='Number of Received Replies (%s)' % ('Passed' if is_passed else 'Failed')
+)
+fig.update_layout(yaxis={'dtick':1})
+fig.show()

2952702d26f1ef2435c2b6f81fdfda75c0cfd84c
yuanyuyuan <az6980522@gmail.com>
2022-08-01 23:12:03 +0800

    update stat_reply_rate for batch exp

--- router-eval-test/utils/stat_reply_rate.py
+++ router-eval-test/utils/stat_reply_rate.py
@@ -7,41 +7,54 @@ import plotly.express as px
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--in-dir',
+    '--exp-dir',
     type=Path,
-    default=Path('./output'),
+    default=Path('./exp/reply-rate'),
 )
 args = parser.parse_args()
 
 
-query_log_files = list(args.in_dir.glob('peer-*/log/query.txt'))
+def load_sample_data(sample_dir: Path):
+    query_log_files = list(sample_dir.glob('peer-*/log/query.txt'))
 
-data = {
-    'peer': [],
-    'reply': [],
-}
-for file in query_log_files:
-    num_peers = int(str(file).split('peer-')[-1].split('/')[0])
-    result = None
-    with open(file) as f:
-        for line in f.readlines():
-            if 'Ended with' in line:
-                result = int(line.split('Ended with ')[-1].split(' peers')[0])
-                break
-    if result is None:
-        print(file, 'contains error!')
-    #  assert result is not None, file
-    data['peer'].append(num_peers)
-    data['reply'].append(result)
+    data = {
+        'peer': [],
+        'reply': [],
+    }
+    for file in query_log_files:
+        num_peers = int(str(file).split('peer-')[-1].split('/')[0])
+        result = None
+        with open(file) as f:
+            for line in f.readlines():
+                if 'Ended with' in line:
+                    result = int(line.split('Ended with ')[-1].split(' peers')[0])
+                    break
+        if result is None:
+            print(file, 'contains error!')
+        #  assert result is not None, file
+        data['peer'].append(num_peers)
+        data['reply'].append(result)
 
-data = pd.DataFrame(data).sort_values(['peer'])
-data['reply'] = data['reply'] / data['peer'] * 100
+    data = pd.DataFrame(data).sort_values(['peer'])
+    data['reply'] = data['reply'] / data['peer'] * 100
+    data['sample'] = str(sample_dir).split('/')[-2]
+    return data
+
+data = pd.concat([
+    load_sample_data(dir / 'outputs')
+    for dir in args.exp_dir.glob('*')
+    if dir.is_dir()
+])
+
 fig = px.line(
     data,
     x='peer',
     y='reply',
+    color='sample',
+    symbol='sample',
     title='Reply Rate',
     markers=True,
+    labels={'sample': 'Exp name'}
 )
 
 fig.update_layout(
@@ -56,4 +69,4 @@ fig.update_layout(
     )
 )
 #  fig.show()
-fig.write_image(args.in_dir / 'reply-rate.png')
+fig.write_image(args.exp_dir / 'reply-rate.png')
